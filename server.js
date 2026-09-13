ddbab7c4b9e9a9bde5bfc10644931bd8d0aa84ab
Pasang Tamang <pasanghilp@gmail.com>
2026-09-13 13:50:45 +0545

    add basic server setup

--- server.js
+++ server.js
@@ -1,2 +1,10 @@
 const express = require('express');
-const 
+const cors = require('cors');
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+let Task = [];
+
+app.listen(3000, () => console.log('Listening on port 5000'));
