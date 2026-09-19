6db16fe0becb0f6ca9efb47791769d6910d3f096
Pasang Tamang <pasanghilp@gmail.com>
2026-09-19 15:21:34 +0545

    Refactor task handling to use MongoDB for CRUD operations

--- server.js
+++ server.js
@@ -1,50 +1,68 @@
 const express = require('express');
 const cors = require('cors');
 
+const Task = require('./task');
+
+require('dotenv').config();
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.MONGO_URI)
+    .then(()=> console.log('Connected to MongoDB'))
+    .catch((err)=> console.log('Connection error', err.message));
+
 const app = express();
 app.use(express.json());
 app.use(cors());
 
 let Tasks = [];
 
-app.get('/tasks',(req,res)=>{
-    res.json(Tasks);
+app.get('/tasks', async (req,res)=>{
+    try {
+        const tasks = await Task.find();
+        res.json(tasks);
+        } catch(e){
+        console.log(e.message);
+        res.status(500).json({ error: "Server error", message: e.message});
+    }
 });
 
-app.post('/tasks',(req,res)=>{
-    const newTask = {
-        id: crypto.randomUUID(),
-        done: false,
-        ...req.body
-    }
-
-    Tasks.push(newTask);
+app.post('/tasks',async (req,res)=>{
+    try {
+    const newTask = await Task.create(req.body);
     res.status(201).json(newTask);
+    } catch(e){
+        console.log(e.message);
+    }
+    });
+
+app.put('/tasks/:id',async (req,res)=>{
+    try {
+        const id = req.params.id;
+        const updatedTask = await Task.findByIdAndUpdate(id, req.body, { returnDocument: 'after', runValidators: true });
+
+        if (!updatedTask) {
+        return res.status(404).json({ error: 'Task not found' });
+        }
+
+        res.json(updatedTask);
+    }
+    catch(e){
+        console.log(e.message);
+    }
 });
 
-app.put('/tasks/:id',(req,res)=>{
-    const id = req.params.id;
-    const taskExists = Tasks.some(task => task.id === id);
-    if (!taskExists) {
-        return res.status(404).json({ error: 'Task not found' });
+app.delete('/tasks/:id',async (req,res)=>{
+    try {
+         const id = req.params.id;
+        const TaskDelete = await Task.findByIdAndDelete(id);
+         if(!TaskDelete){
+            return res.status(404).json({ error: "Task not found"});
     }
-    
-    Tasks = Tasks.map(Task => Task.id === id? {
-        ...Task, ...req.body
-    }: Task)
-    const updatedTask = Tasks.find(t => t.id === id);
-    res.json(updatedTask);
-});
-
-app.delete('/tasks/:id',(req,res)=>{
-    const id = req.params.id;
-
-    const taskExists = Tasks.some(task => task.id === id);
-    if (!taskExists) {
-        return res.status(404).json({ error: 'Task not found' });
+    res.json(TaskDelete);
     }
-    Tasks = Tasks.filter(Task => Task.id !== id);
-    res.json(Tasks);
+    catch (e){
+        console.log(e.message);
+    }
 });
 
 const PORT = process.env.PORT || 3000;
